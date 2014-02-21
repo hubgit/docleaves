727500412917b4d9fe07e9b25f33a9244457e1f8
Alf Eaton <alf@hubmed.org>
2014-02-21 14:45:08 +0000

    initial commit

---
+++ docleaves.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function ready(data){
+	var nodes = document.querySelectorAll('script[type="text/eval+javascript"]');
+
+	Array.prototype.forEach.call(nodes, function(node) {
+		var script = node.innerHTML.trim();
+
+		var figure = document.createElement('figure');
+		node.parentNode.insertBefore(figure, node);
+		node.parentNode.removeChild(node);
+
+		var code = document.createElement('code');
+		code.className = 'prettyprint lang-js';
+		code.innerHTML = script;
+		figure.appendChild(code);
+
+		var samp = document.createElement('samp');
+		figure.appendChild(samp);
+
+		(function(script, node) {
+			try {
+				var output = new Function('data', script)(data);
+
+				if (output instanceof HTMLElement) {
+					samp.appendChild(output);
+				} else {
+					samp.textContent = JSON.stringify(output, null, 2);
+				}
+			} catch (e) {
+				samp.className = 'error';
+				samp.textContent = e.message;
+			}
+		})(code.textContent, samp);
+	});
+
+	prettyPrint();
+}
