795ca94a74e2511abfa2e30ae30a54be8fef1cde
Alf Eaton <alf@hubmed.org>
2014-02-23 17:56:34 +0000

    load requirements

--- docleaves.js
+++ docleaves.js
@@ -4,63 +4,104 @@ if (typeof window.Promise != 'function') {
 	alert('This requires a browser that supports ES6 Promises, e.g. Chrome 32');
 }
 
+var scripts = document.getElementsByTagName('script');
+
 var docleaves = {
-	ready: function(){
-		prettyPrint(); // convert the Markown first
+	queue: [],
+	base: scripts[scripts.length - 1].getAttribute('src').replace(/\/[^\/]*$/, '/'),
+	requirements: [
+		'vendor/d3/d3.js',
+		'vendor/marked/lib/marked.js',
+		'vendor/codemirror/lib/codemirror.js',
+		'vendor/codemirror/mode/javascript/javascript.js',
+		'vendor/highlight/highlight.pack.js'
+	],
+	convert: function(node) {
+		console.log('convert');
+		node.innerHTML = marked(node.innerHTML);
+		node.style.display = 'block';
+	},
+	prepare: function(node){
+		var figure = document.createElement('figure');
+		node.parentNode.insertBefore(figure, node);
 
-		var nodes = document.querySelectorAll('script[type="text/eval+javascript"]');
+		var textarea = document.createElement('textarea');
+		textarea.innerHTML = node.innerHTML.trim();;
+		figure.appendChild(textarea);
 
-		var queue = Array.prototype.slice.call(nodes);
+		var editor = CodeMirror.fromTextArea(textarea, {
+		    mode: 'javascript',
+		    lineNumbers: true,
+		    theme: 'blackboard'
+		});
 
-		var run = function() {
-			var node = queue.shift();
-			var script = node.innerHTML.trim();
-			var placeholder = node.getAttribute('placeholder');
+		var samp = document.createElement('samp');
+		samp.textContent = node.getAttribute('placeholder');
+		figure.appendChild(samp);
 
-			var figure = document.createElement('figure');
-			node.parentNode.insertBefore(figure, node);
-			node.parentNode.removeChild(node);
+		node.parentNode.removeChild(node);
 
-			var code = document.createElement('code');
-			code.className = 'prettyprint lang-js';
-			code.innerHTML = script;
-			figure.appendChild(code);
+		docleaves.queue.push(figure);
+	},
 
-			var samp = document.createElement('samp');
-			samp.textContent = placeholder;
-			figure.appendChild(samp);
-
-			var error = function(message) {
-				samp.className = 'error';
-				done(message);
-			};
-
-			var done = function(output) {
-				if (output instanceof Promise) {
-					return output.then(done, error);
-				} else if (output instanceof HTMLElement) {
-					samp.appendChild(output);
-				} else {
-					samp.textContent = JSON.stringify(output, null, 2);
-				}
-
-				if (queue.length) {
-					run();
-				} //else {
-					prettyPrint();
-				//}
-			};
-
-			(function(script, node) {
-				try {
-					done(new Function(script)());
-				} catch (e) {
-					error(e.message);
-				}
-			})(code.textContent, samp);
+	run: function() {
+		if (!docleaves.queue.length) {
+			return;
 		}
 
-		run();
+		var figure = docleaves.queue.shift();
+		var samp = figure.querySelector('samp');
+		var script = figure.querySelector('textarea').textContent;
+
+		var error = function(message) {
+			samp.className = 'error';
+			done(message);
+		};
+
+		var done = function(output) {
+			samp.className = 'success';
+			if (output instanceof Promise) {
+				return output.then(done, error);
+			} else if (output instanceof HTMLElement) {
+				samp.appendChild(output);
+			} else {
+				samp.textContent = JSON.stringify(output, null, 2);
+				hljs.highlightBlock(samp);
+			}
+
+			docleaves.run();
+		};
+
+		(function(script, node) {
+			samp.className = 'running';
+
+			try {
+				done(new Function(script)());
+			} catch (e) {
+				error(e.message);
+			}
+		})(script, samp);
+	},
+
+	ready: function() {
+		// convert the Markdown
+		Array.prototype.forEach.call(document.querySelectorAll('.markdown'), docleaves.convert);
+
+		// prepare each leaf
+		Array.prototype.forEach.call(document.querySelectorAll('.leaf'), docleaves.prepare);
+
+		// run each script
+		docleaves.run();
+	},
+
+	require: function(callback) {
+		var script = document.createElement('script');
+		script.src = docleaves.base + docleaves.requirements.shift();
+		script.onload = docleaves.requirements.length ? docleaves.require : docleaves.ready;
+		document.body.appendChild(script);
 	}
 };
 
+document.addEventListener('DOMContentLoaded', function() {
+	docleaves.require();
+});
